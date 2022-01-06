e3221eadc0885416cd7538bd100abe4f16f49cb0
joshua360x <jkw5499@psu.edu>
2022-01-05 20:29:52 -0500

    general function for todo + connected to DB

--- todos/todos.js
+++ todos/todos.js
@@ -16,20 +16,54 @@ const logoutButton = document.querySelector('#logout');
 const deleteButton = document.querySelector('.delete-button');
 
 todoForm.addEventListener('submit', async(e) => {
+    e.preventDefault();
     // on submit, create a todo, reset the form, and display the todos
+    const data = new FormData(todoForm);
+    const toDoInput = data.get('todo');
+    await createTodo(toDoInput);
+    displayTodos();
 });
+
 
 async function displayTodos() {
     // fetch the todos
+    const toDoData = await getTodos();
+    // eslint-disable-next-line no-console
+    console.log('🚀 ~ file: todos.js ~ line 30 ~ displayTodos ~ toDoData', toDoData);
+
+    todosEl.textContent = '';
+    toDoData.forEach(eachToDo => {
+    //    console.log(eachToDo.todo)
+    // const toDoItem = document.createElement('p')
+    // toDoItem.textContent = eachToDo.todo;
+
     
+    
+        const newToDo = renderTodo(eachToDo);
+    
+        newToDo.addEventListener('click', async() => {
+            await completeTodo(eachToDo.id);
+            displayTodos();
+        });
+
+
+        todosEl.append(newToDo);
+    });
     // display the list of todos
-
     // be sure to give each todo an event listener
-
+    
     // on click, complete that todo
+
+
+    // for (const eachToDo of toDoData.todo) {
+    //     console.log(eachToDo);
+    // }
 }
 
 // add an on load listener that fetches and displays todos on load
+window.addEventListener('load', () => {
+    displayTodos();
+});
 
 logoutButton.addEventListener('click', () => {
     logout();
@@ -38,6 +72,9 @@ logoutButton.addEventListener('click', () => {
 
 deleteButton.addEventListener('click', async() => {
     // delete all todos
-
+    await deleteAllTodos();
     // then refetch and display the updated list of todos
+    displayTodos();
 });
+
+
